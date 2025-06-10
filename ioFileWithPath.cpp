45d9c7b2600c50c55e801d75cc63eecab351066c
ZakyRizqiHasanain <zaky.rizqi.ft24@mail.umy.ac.id>
2025-06-10 18:12:05 +0700

    membuat jenis data dan memasukkan nama file menggunakan cin

---
+++ ioFileWithPath.cpp
@@ -0,0 +1,13 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string baris;
+    string namafile;
+    cout << "Masukkan nama file : ";
+    cin >> namafile;
+    
+
+}
